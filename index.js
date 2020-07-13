6d8e9f40410f097f016b97371ef5e141f22a5067
abdulloooh <abdullahakinwumi@gmail.com>
2020-07-13 20:19:09 +0100

    added diff of two squares

--- index.js
+++ index.js
@@ -14,7 +14,12 @@ function multiply(...param) {
   return product;
 }
 
+function differenceOf2Squares(a, b) {
+  return multiply(a, a) - multiply(b, b);
+}
+
 module.exports = {
   add: add,
   multiply: multiply,
+  squareDifference: differenceOf2Squares,
 };
